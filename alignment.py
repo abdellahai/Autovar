e6627b22f75da3bbc7f8d0a981f988fd03f140a1
abdellahia <abdellah_idrissiazami@um5.ac.ma>
2021-02-02 13:36:51 +0100

    'version 3'

--- alignment.py
+++ alignment.py
@@ -1,5 +1,5 @@
 import argparse as ag
-from Bio.Sequencing.Applications import BwaIndexCommandline, BwaIndexCommandline, SamtoolsCalmdCommandline, SamtoolsMpileupCommandline, SamtoolsSortCommandline, SamtoolsViewCommandline
+from Bio.Sequencing.Applications import BwaIndexCommandline, BwaMemCommandline, SamtoolsCalmdCommandline, SamtoolsMpileupCommandline, SamtoolsSortCommandline, SamtoolsViewCommandline
 import pandas as pd
 import pandas_genomics
 import scipy as sp
@@ -9,6 +9,7 @@ import vcf
 from datetime import datetime
 import time
 import os
+from shutil import copyfile
 parser = ag.ArgumentParser(description='GSA and Variant calling')
 parser.add_argument('Input',help='Input sequence(s)', type = str, nargs="+")
 parser.add_argument('Outdir', help='Output directory (deafault = current directory)',type=str,default='.')
@@ -18,8 +19,12 @@ args = parser.parse_args()
 input_sequences = args.Input
 output = args.Outdir
 ref = args.Ref
-os.mkdir(f'{output}/refrence')
+os.mkdir(f'{output}/ref')
 os.mkdir(f'{output}/raw_sams')
 os.mkdir(f'{output}/sorted_sams')
 os.mkdir(f'{output}/bams')
+copyfile(ref,f'{output}/ref/ref.fasta')
+ref = f'{output}/ref/ref.fasta'
+cmd = BwaIndexCommandline (infile=ref)
+cmd()
 
