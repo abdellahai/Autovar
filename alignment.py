a347f00251a01d03ddd5a705b577d61423a024c3
abdellahia <abdellah_idrissiazami@um5.ac.ma>
2021-02-02 13:30:47 +0100

    Second version

--- alignment.py
+++ alignment.py
@@ -8,12 +8,18 @@ import numpy as np
 import vcf
 from datetime import datetime
 import time
+import os
 parser = ag.ArgumentParser(description='GSA and Variant calling')
 parser.add_argument('Input',help='Input sequence(s)', type = str, nargs="+")
 parser.add_argument('Outdir', help='Output directory (deafault = current directory)',type=str,default='.')
 parser.add_argument('-v', '--verbose',help="increase output verbosity", action="store_true")
+parser.add_argument('Ref', help= 'Reference sequence', type = 'str')
 args = parser.parse_args()
 input_sequences = args.Input
-print(input_sequences)
-if args.verbose:
-    print("verbosity turned on")
+output = args.Outdir
+ref = args.Ref
+os.mkdir(f'{output}/refrence')
+os.mkdir(f'{output}/raw_sams')
+os.mkdir(f'{output}/sorted_sams')
+os.mkdir(f'{output}/bams')
+
